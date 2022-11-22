540e3462b8c22635ee8d73057bdb52f1906dfe5e
wilinz <weiizan71@gmail.com>
2022-11-22 14:15:31 +0800

    Fixed: Fixed handling of files containin CRLF lines endings codejive/java-properties#15

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,8 +39,8 @@ dependencies {
 //    compileOnly(compose.desktop.currentOs) runtime dependency is provided by org.jetbrains.compose.intellij.platform
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    implementation("com.google.code.gson:gson:2.9.0")
-    implementation("com.github.wilinz:java-properties:1.0.6")
+    implementation("com.google.code.gson:gson:2.10")
+    implementation("com.github.wilinz:java-properties:1.0.7")
     implementation("com.github.dom4j:dom4j:version-2.1.1")
     testImplementation(kotlin("test"))
 }
